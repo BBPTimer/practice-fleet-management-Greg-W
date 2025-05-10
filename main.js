d0931d19469ced8de96f7b50cbae9d2de775eb07
gweseloh <greg.weseloh@gmail.com>
2025-05-10 09:04:08 -0500

    Updated default value after lesson

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 class Vehicle {
-    constructor(type, make, model, year) {
+    constructor(type, make, model, year, mileage = 0) {
         this.type = type;
         this.make = make;
         this.model = model;
         this.year = year;
-        this.mileage = 0;
+        this.mileage = mileage;
     }
 
     drive(distance) {
